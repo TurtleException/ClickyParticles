2e50d78989e732cbb72fd55b7e9f7d3c3fa1b873
TurtleException <turtle.exception@eldritch.de>
2022-05-29 01:51:38 +0200

    Initial commit

---
+++ src/main/java/de/eldritch/spigot/clicky_particles/ClickyParticles.java
@@ -0,0 +1,10 @@
+package de.eldritch.spigot.clicky_particles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ClickyParticles extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // TODO
+    }
+}
